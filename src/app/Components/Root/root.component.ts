8e978ef1f74629f4290c723be33c1f0e98f5f979
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2025-05-14 14:02:40 +0200

    Add files via upload

---
+++ src/app/Components/Root/root.component.ts
@@ -0,0 +1,83 @@
+/// <reference types="chrome"/>
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { HeaderComponent } from "../Header/header.component";
+import { SearchComponent } from "../Search/search.component";
+import { LoadComponent } from "../Load/load.component";
+import { ExportComponent } from "../Export/export.component";
+import { DEImportComponent } from "../DEImport/deimport.component";
+import { ResultsComponent } from "../Results/results.component";
+import { Utility } from "../../Logics/utility";
+
+@Component({
+  selector: "app-root",
+  imports: [HeaderComponent, SearchComponent, LoadComponent, ExportComponent, DEImportComponent, ResultsComponent],
+  templateUrl: "./root.component.html",
+  styleUrl: "./root.component.css"
+})
+
+export class RootComponent {
+  public name!: string;
+  public version!: string;
+  public BUname: string | undefined;
+  public BUid: string | undefined;
+  public showSearch = true;
+  public showLoad = false;
+  public showExport = false;
+  public showDEImport = false;
+
+  @ViewChild("results")
+  private readonly results!: ResultsComponent;
+
+  @ViewChild("search") 
+  private readonly search!: SearchComponent;
+
+  @ViewChild("export")
+  private readonly export!: SearchComponent;
+
+  private async GetBUData(): Promise<any> {
+    try {
+      return await Utility.GetSiteBUData();
+    }
+    catch(err: any) {
+      console.log(err);
+    }
+  }
+
+  public async ngAfterViewInit(): Promise<void> {
+    const manifest: chrome.runtime.Manifest = chrome.runtime.getManifest();
+    const data: any = await this.GetBUData();
+
+    this.name = manifest.name;
+    this.version = manifest.version;
+    this.BUname = data?.BUname;
+    this.BUid = data?.BUid;
+  }
+
+  public OnHeaderEmitted(flags: any): void {
+    this.showSearch = flags.showSearch;
+    this.showLoad = flags.showLoad;
+    this.showExport = flags.showExport;
+    this.showDEImport = flags.showDEImport;
+  }
+
+  public async OnExportEmitted(inp: any): Promise<void> {
+    switch(true) {
+      case inp.export:
+        await this.results.Export();
+        break;
+
+      case inp.clear:
+        await this.search.InitBUs();
+        break;
+    } 
+  }
+
+  public async OnLoadEmitted(): Promise<void> {
+    await this.search.InitBUs();
+    await this.export.InitBUs();
+  }
+
+  public OnSearchEmitted(res: any): void {
+    this.results.Populate(res);
+  }
+}
---
+++ src/app/Components/Root/root.config.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./root.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes)]
+};
