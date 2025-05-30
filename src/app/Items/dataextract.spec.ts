a2bde4959e064395a181141cccf8682a6130da72
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2025-05-30 15:03:35 +0200

    Update and rename datatextract.spec.ts to dataextract.spec.ts

---
+++ src/app/Items/dataextract.spec.ts
@@ -0,0 +1,7 @@
+import { DataExtract } from "./dataextract";
+
+describe("DataExtract", () => {
+  it("should create an instance", () => {
+    expect(new DataExtract()).toBeTruthy();
+  });
+});
--- src/app/Items/datatextract.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Datatextract } from "./datatextract";
-
-describe("Datatextract", () => {
-  it("should create an instance", () => {
-    expect(new Datatextract()).toBeTruthy();
-  });
-});
