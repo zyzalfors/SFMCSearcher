61fcb299b9c8ab2f6c4d53a91c7dbf19f213468a
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2026-01-08 14:50:52 +0100

    Delete src/app/Items/journeyhistoryentry.spec.ts

--- src/app/Items/journeyhistoryentry.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JourneyHistoryEntry } from "./journeyhistoryentry";
-
-describe("JourneyHistoryEntry", () => {
-  it("should create an instance", () => {
-    expect(new JourneyHistoryEntry()).toBeTruthy();
-  });
-});
