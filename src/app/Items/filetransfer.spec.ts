3d25a0938dcbb91a0553351a70b4c36f032e26e3
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2026-01-08 14:51:45 +0100

    Delete src/app/Items/filetransfer.spec.ts

--- src/app/Items/filetransfer.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FileTransfer } from "./filetransfer";
-
-describe("FileTransfer", () => {
-  it("should create an instance", () => {
-    expect(new FileTransfer()).toBeTruthy();
-  });
-});
