b63d0d2070fd27a44c69148cf32f716d0b129049
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2026-01-08 14:51:24 +0100

    Delete src/app/Items/filterdefinition.spec.ts

--- src/app/Items/filterdefinition.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FilterDefinition } from "./filterdefinition";
-
-describe("FilterDefinition", () => {
-  it("should create an instance", () => {
-    expect(new FilterDefinition()).toBeTruthy();
-  });
-});
